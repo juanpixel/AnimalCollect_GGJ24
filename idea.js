64ad0b8bf378e5524940232b538084d995c29ea6
JuanPixel <juanmunozrozo@gmail.com>
2024-02-05 18:51:50 -0500

    ajuste en la lógica del videojuego

--- idea.js
+++ idea.js
@@ -1,42 +1,310 @@
-window.addEventListener('keydown', moveKeyBoard);
+// Aparece una imagen en pantalla
+// inicia el tiempo regresivo = a tiempoElección
+// si tiempoElección llega a 0 Jugador 1 y 2 pierden
+// el sistema revisa cada 100ms si jugador 1 presiono "q" o "w" o "e" y si jugador 2 presiono "arrowLwft" o "arrowDown" o "arrowRigth"
+// si jugador 1 y jugador 2 seleccionaron alguna tecla indicada el sistema revisa si jugador 1 y jugador 2 acertaron
+// si el jugador 1 no acerto el sistema indica que peridio el jugador 1
+// si el jugador 2 no acerto elsistama indica que perdio jugador 2
+// si ninguno acierta el sistema indica que ambos jugadores perdieron
+// si ambos jugadores aciertan tiempoelección se multipica por 0.95, puntos++ y se inicia el juego nuevamente 
 
-let teclas = []
+const btnVolver = document.querySelector('#volver');
+
+const btnOveja = document.querySelector('#oveja');
+const btnVaca = document.querySelector('#vaca');
+const btnPollo = document.querySelector('#pollo');
+
+const btnOveja2 = document.querySelector('#oveja2');
+const btnVaca2 = document.querySelector('#vaca2');
+const btnPollo2 = document.querySelector('#pollo2');
+
+const setPuntos = document.querySelector('#puntos');
+const setPRecord = document.querySelector('#record');
+const perdisteSection = document.querySelector('#perdiste');
+
+const vida = document.querySelector('#Vida');
+
+let jaja = new Audio('./Sound/jaja.mp3');
+let pop = new Audio('./Sound/pop.mp3');
+
+let vidaRestante = 100;
+vida.style.width = `${vidaRestante}%`;
+
+let juegoActivo = true
+let tiempo = 3000
+let seleccion1 = null
+let seleccion2 = null
+let jugador1 = null
+let jugador2 = null
+let revisando
+
+const imagenes = [
+  {
+      url: "./IMG/bomba.png",
+      name: 'bomba'
+  },
+  {
+      url: "./IMG/vaca.png",
+      name: 'vaca'
+  },
+  {
+      url: "./IMG/oveja.png",
+      name: 'oveja'
+  },
+  {
+      url: "./IMG/pollo.png",
+      name: 'pollo'
+  },
+];
+
+const controlesJugador1 = ['q','w','e']
+const controlesJugador2 = ['ArrowLeft','ArrowDown','ArrowRight']
+
+window.addEventListener('keydown', seleccionRespuesta)
+
+btnVolver.addEventListener('click', cerrar);
 
 
-function moveKeyBoard(key){
+btnOveja.addEventListener('click', () => {
+    pop.play();
+    jugador1 = 'oveja'
+    seleccion1 = 'oveja'
+    seleccionarRespuestaTouch()
+});
+
+btnVaca.addEventListener('click', () =>{
+  pop.play();
+  jugador1 = 'vaca'
+  seleccion1 = 'vaca'
+  seleccionarRespuestaTouch()
+});
+
+btnPollo.addEventListener('click', () =>{
+  pop.play();
+  jugador1 = 'pollo'
+  seleccion1 = 'pollo'
+  seleccionarRespuestaTouch()
+});
+btnOveja2.addEventListener('click', function () {
+  pop.play();
+  jugador2 = 'oveja'
+  seleccion2 = 'oveja'
+  seleccionarRespuestaTouch()
+});
+btnVaca2.addEventListener('click', function () {
+  pop.play();
+  jugador2 = 'vaca'
+  seleccion2 = 'vaca'
+  seleccionarRespuestaTouch()
+});
+
+btnPollo2.addEventListener('click', function () {
+  pop.play();
+  jugador2 = 'pollo'
+  seleccion2 = 'pollo'
+  seleccionarRespuestaTouch()
+})
+
+
+
+function revisarTiempo(){ // Esta función hace la cuenta de tiempo 
+
+  mostrarImagen()
+
+  console.log(imageName)
+
+  let TiempoInicio = Date.now()
+
+  const decrementoPorIntervalo = (100 / (tiempo / 100));
+  
+  console.log(TiempoInicio)
+
+  revisando = setInterval( 
+    ()=>{
+      let tiempoTransurrido =  Date.now() - TiempoInicio
+
+      console.log(tiempoTransurrido)
+
+      console.log(jugador1,jugador2)
+      
+      vida.style.width = `${vidaRestante}%`;
+
+      vidaRestante -= decrementoPorIntervalo;
+
+      if(tiempoTransurrido >= tiempo){
+
+        if(imageName === 'bomba'){
+          console.log('Era Dr. bomby')
+          clearInterval(revisando)
+          vidaRestante = 100
+          vida.style.width = `${vidaRestante}%`
+          revisarTiempo()
+        }else{
+          if (!jugador1  && !jugador2 ){
+            setPRecord.innerHTML = "Ninguno selecciono";
+            perdiste()
+          }else if(jugador1 && !jugador2){
+            setPRecord.innerHTML = "Jugador 2";
+            perdiste()
+            console.log('jugador 2 no selecciono')
+          }else if (!jugador1 && jugador2){
+            setPRecord.innerHTML = "Jugador 1";
+            perdiste()
+            console.log('jugador 1 no selecciono')
+          }
+
+          clearInterval(revisando)
+          juegoActivo = false
+        }
+
+      }
+
+    },100
+  )
+}
+
+function seleccionRespuesta(event){ //esta función guarda en una variable cada selección del jugador 
+
+  console.log(event.key)
+
+ if(controlesJugador1.includes(event.key)){
+    jugador1 = event.key
+ }else if(controlesJugador2.includes(event.key)){
+    jugador2 = event.key
+ }
+
+ if(jugador1 && jugador2){
+   console.log('selección realizada ') 
+   clearInterval(revisando)
+   revisarSeleccion();
+ }
+}
+
+function seleccionarRespuestaTouch(){
+  if(jugador1 && jugador2){
+    console.log('selección realizada ') 
+    clearInterval(revisando)
+    revisarSeleccion();
+  }
+}
+
+
+function revisarSeleccion(){
+  let jugador1Gana = false
+  let jugador2Gana = false
+
+  switch(jugador1){
+    case 'q':
+      pop.play();
+      seleccion1 = 'oveja'
+      break
     
-    console.log(key.key)
-    teclas.push(key.key)     
-}
+    case "w" :
+      pop.play();
+      seleccion1 = 'vaca'
+      break
 
-function gano(){
-    console.log(teclas)
+    case "e":
+      pop.play();
+      seleccion1 = 'pollo'
+  }
 
-    if(teclas.length > 2){
-        teclas.splice(2,teclas.length)
-    }
-
-    console.log(teclas)
-
-    if(teclas[0]=== 'q' && teclas[1]=== 'w' || teclas[1]=== 'q' && teclas[0]=== 'w'){
-        console.log('ganaste')
-        juego();
-    }else{
-        console.log('perdite');
-        
-    }
-
-    teclas.splice(0,teclas.length)
-}
-
-function juego(){
-
-
-        console.log('Selecciona una imagen')
+  switch(jugador2){
+    case 'ArrowLeft':
+      pop.play();
+      seleccion2 = 'oveja'
+      break
     
-        setTimeout(() => {
-            gano();
-        },5000);
+    case "ArrowDown" :
+      pop.play();
+      seleccion2 = 'vaca'
+      break
 
+    case "ArrowRight":
+      pop.play();
+      seleccion2 = 'pollo'
+  }
+
+  console.log(seleccion1 + "ademas de " +seleccion2)
+
+  if(seleccion1 === imageName){
+    jugador1Gana = true
+    
+
+  }
+
+  if(seleccion2 === imageName){
+    jugador2Gana = true
+    
+  
+  }
+
+  if(jugador1Gana && jugador2Gana){
+    jugador1Gana = false
+    jugador2Gana = false
+    jugador1 = null
+    jugador2 = null
+    tiempo = tiempo*0.95
+    vidaRestante = 100
+    vida.style.width = `${vidaRestante}%`
+    iniciarJuego()
+  }else if(jugador1Gana && !jugador2Gana){
+    juegoActivo = false
+    setPRecord.innerHTML = "Jugador 2";
+    perdiste()
+    console.log('Jugador 2 pierde')
+    
+  }else if(!jugador1Gana && jugador2Gana){
+    juegoActivo = false
+    setPRecord.innerHTML = "Jugador ";
+    perdiste()
+  }else{
+    juegoActivo = false
+    setPRecord.innerHTML = "Ambos perdieron";
+    perdiste()
+  }
 }
-juego();
+
+function mostrarImagen(){
+  const imagenElement = document.querySelector(".display img");
+  imagenElement.src = "./IMG/pregunta.png";
+  imageName = 'pregunta';
+
+  setTimeout(
+    ()=>{
+      const randomIndex = Math.floor(Math.random() * imagenes.length);
+        imagenElement.src = imagenes[randomIndex].url;
+        imageName = imagenes[randomIndex].name;
+        console.log(imageName);
+    },200
+  )
+}
+
+
+
+function iniciarJuego(){
+  
+  if(juegoActivo){
+    console.log("mostrado imagen")
+    revisarTiempo()
+  }else{
+    console.log('juego parado')
+  }
+}
+
+function perdiste() {
+  jaja.play();
+
+  perdisteSection.classList.remove('oculto'); 
+}
+
+function cerrar() {
+  perdisteSection.classList.add('oculto');
+  juegoActivo = true
+  clearInterval(revisando)
+  vidaRestante = 100
+  vida.style.width = `${vidaRestante}%`
+  iniciarJuego()
+}
+
+iniciarJuego();
--- main2.js
+++ main2.js
@@ -1,5 +1,14 @@
-const btnVolver = document.querySelector('#volver');
+// Aparece una imagen en pantalla
+// inicia el tiempo regresivo = a tiempoElección
+// si tiempoElección llega a 0 Jugador 1 y 2 pierden
+// el sistema revisa cada 100ms si jugador 1 presiono "q" o "w" o "e" y si jugador 2 presiono "arrowLwft" o "arrowDown" o "arrowRigth"
+// si jugador 1 y jugador 2 seleccionaron alguna tecla indicada el sistema revisa si jugador 1 y jugador 2 acertaron
+// si el jugador 1 no acerto el sistema indica que peridio el jugador 1
+// si el jugador 2 no acerto elsistama indica que perdio jugador 2
+// si ninguno acierta el sistema indica que ambos jugadores perdieron
+// si ambos jugadores aciertan tiempoelección se multipica por 0.95, puntos++ y se inicia el juego nuevamente 
 
+const btnVolver = document.querySelector('#volver');
 
 const btnOveja = document.querySelector('#oveja');
 const btnVaca = document.querySelector('#vaca');
@@ -9,187 +18,334 @@ const btnOveja2 = document.querySelector('#oveja2');
 const btnVaca2 = document.querySelector('#vaca2');
 const btnPollo2 = document.querySelector('#pollo2');
 
-
-const setPuntos = document.querySelector('#puntos')
-const setPRecord = document.querySelector('#record')
+const setPuntos = document.querySelector('#puntos');
+const setPRecord = document.querySelector('#record');
 const perdisteSection = document.querySelector('#perdiste');
 
+const vida = document.querySelector('#Vida');
+
+let jaja = new Audio('./Sound/jaja.mp3');
+let pop = new Audio('./Sound/pop.mp3');
+
+let vidaRestante = 100;
+vida.style.width = `${vidaRestante}%`;
+
+let juegoActivo = true
+let tiempo = 3000
+let seleccion1 = null
+let seleccion2 = null
+let jugador1 = null
+let jugador2 = null
+let revisando
+
 const imagenes = [
-    {
-        url: "./IMG/bomba.png",
-        name: 'bomba'
-    },
-    {
-        url: "./IMG/vaca.png",
-        name: 'vaca'
-    },
-    {
-        url: "./IMG/oveja.png",
-        name: 'oveja'
-    },
-    {
-        url: "./IMG/pollo.png",
-        name: 'pollo'
-    },
+  {
+      url: "./IMG/bomba.png",
+      name: 'bomba'
+  },
+  {
+      url: "./IMG/vaca.png",
+      name: 'vaca'
+  },
+  {
+      url: "./IMG/oveja.png",
+      name: 'oveja'
+  },
+  {
+      url: "./IMG/pollo.png",
+      name: 'pollo'
+  },
 ];
 
-let puntos = 0;
-let tiempoEleccion = 3000; // 3 segundos para elegir por defecto
-let imageName;
-let intervalId; // Variable para almacenar el ID del intervalo
-let juegoActivo = true;
-let botonPerdido;
+const controlesJugador1 = ['q','w','e']
+const controlesJugador2 = ['ArrowLeft','ArrowDown','ArrowRight']
 
-let jaja = new Audio('./Sound/jaja.mp3')
-let pop = new Audio('./Sound/pop.mp3')
-
+window.addEventListener('keydown', seleccionRespuesta)
 
 btnVolver.addEventListener('click', cerrar);
 
-function cerrar() {
-    perdisteSection.classList.add('oculto');
-    setPRecord.innerHTML = "0"
-    juegoActivo = true
-    puntos = 0
-    cambiarImagenAleatoria()
+
+btnOveja.addEventListener('click', () => {
+    pop.play();
+    jugador1 = 'oveja'
+    seleccion1 = 'oveja'
+    seleccionarRespuestaTouch()
+});
+
+btnVaca.addEventListener('click', () =>{
+  pop.play();
+  jugador1 = 'vaca'
+  seleccion1 = 'vaca'
+  seleccionarRespuestaTouch()
+});
+
+btnPollo.addEventListener('click', () =>{
+  pop.play();
+  jugador1 = 'pollo'
+  seleccion1 = 'pollo'
+  seleccionarRespuestaTouch()
+});
+btnOveja2.addEventListener('click', function () {
+  pop.play();
+  jugador2 = 'oveja'
+  seleccion2 = 'oveja'
+  seleccionarRespuestaTouch()
+});
+btnVaca2.addEventListener('click', function () {
+  pop.play();
+  jugador2 = 'vaca'
+  seleccion2 = 'vaca'
+  seleccionarRespuestaTouch()
+});
+
+btnPollo2.addEventListener('click', function () {
+  pop.play();
+  jugador2 = 'pollo'
+  seleccion2 = 'pollo'
+  seleccionarRespuestaTouch()
+})
+
+
+/// on touch
+btnOveja.addEventListener('touchstart', () => {
+    pop.play();
+    jugador1 = 'oveja'
+    seleccion1 = 'oveja'
+    seleccionarRespuestaTouch()
+});
+
+btnVaca.addEventListener('touchstart', () =>{
+  pop.play();
+  jugador1 = 'vaca'
+  seleccion1 = 'vaca'
+  seleccionarRespuestaTouch()
+});
+
+btnPollo.addEventListener('touchstart', () =>{
+  pop.play();
+  jugador1 = 'pollo'
+  seleccion1 = 'pollo'
+  seleccionarRespuestaTouch()
+});
+btnOveja2.addEventListener('touchstart', function () {
+  pop.play();
+  jugador2 = 'oveja'
+  seleccion2 = 'oveja'
+  seleccionarRespuestaTouch()
+});
+btnVaca2.addEventListener('touchstart', function () {
+  pop.play();
+  jugador2 = 'vaca'
+  seleccion2 = 'vaca'
+  seleccionarRespuestaTouch()
+});
+
+btnPollo2.addEventListener('touchstart', function () {
+  pop.play();
+  jugador2 = 'pollo'
+  seleccion2 = 'pollo'
+  seleccionarRespuestaTouch()
+})
+
+
+function revisarTiempo(){ // Esta función hace la cuenta de tiempo 
+
+  mostrarImagen()
+
+  console.log(imageName)
+
+  let TiempoInicio = Date.now()
+
+  const decrementoPorIntervalo = (100 / (tiempo / 100));
+  
+  console.log(TiempoInicio)
+
+  revisando = setInterval( 
+    ()=>{
+      let tiempoTransurrido =  Date.now() - TiempoInicio
+
+      console.log(tiempoTransurrido)
+
+      console.log(jugador1,jugador2)
+      
+      vida.style.width = `${vidaRestante}%`;
+
+      vidaRestante -= decrementoPorIntervalo;
+
+      if(tiempoTransurrido >= tiempo){
+
+        if(imageName === 'bomba'){
+          console.log('Era Dr. bomby')
+          clearInterval(revisando)
+          vidaRestante = 100
+          vida.style.width = `${vidaRestante}%`
+          revisarTiempo()
+        }else{
+          if (!jugador1  && !jugador2 ){
+            setPRecord.innerHTML = "Ninguno selecciono";
+            perdiste()
+          }else if(jugador1 && !jugador2){
+            setPRecord.innerHTML = "Jugador 2";
+            perdiste()
+            console.log('jugador 2 no selecciono')
+          }else if (!jugador1 && jugador2){
+            setPRecord.innerHTML = "Jugador 1";
+            perdiste()
+            console.log('jugador 1 no selecciono')
+          }
+
+          clearInterval(revisando)
+          juegoActivo = false
+        }
+
+      }
+
+    },100
+  )
 }
 
-function iniciarJuego() {
-    if(juegoActivo){
-        cambiarImagenAleatoria();
+function seleccionRespuesta(event){ //esta función guarda en una variable cada selección del jugador 
 
-        // Event listeners para los botones
-        btnOveja.addEventListener('click', function () {
-            pop.play()
-            VerificarImagen(btnOveja.id);
-        });
-    
-        btnVaca.addEventListener('click', function () {
-            pop.play()
-            VerificarImagen(btnVaca.id);
-        });
-    
-        btnPollo.addEventListener('click', function () {
-            pop.play()
-            VerificarImagen(btnPollo.id);
-        })
+  console.log(event.key)
 
-        btnOveja2.addEventListener('click', function () {
-            pop.play()
-            VerificarImagen(btnOveja2.id);
-        });
-    
-        btnVaca2.addEventListener('click', function () {
-            pop.play()
-            VerificarImagen(btnVaca2.id);
-        });
-    
-        btnPollo2.addEventListener('click', function () {
-            pop.play()
-            VerificarImagen(btnPollo2.id);
-        })
-    }
+ if(controlesJugador1.includes(event.key)){
+    jugador1 = event.key
+ }else if(controlesJugador2.includes(event.key)){
+    jugador2 = event.key
+ }
 
+ if(jugador1 && jugador2){
+   console.log('selección realizada ') 
+   clearInterval(revisando)
+   revisarSeleccion();
+ }
 }
 
-function cambiarImagenAleatoria() {
-    reiniciarIntervalo(); // Reiniciar el intervalo antes de cambiar la imagen
+function seleccionarRespuestaTouch(){
+  if(jugador1 && jugador2){
+    console.log('selección realizada ') 
+    clearInterval(revisando)
+    revisarSeleccion();
+  }
+}
 
-    // Mostrar la imagen de pregunta antes de cambiar a una imagen aleatoria
-    const imagenElement = document.querySelector(".display img");
-    imagenElement.src = "./IMG/pregunta.png";
-    imageName = 'pregunta';  // Asignar un nombre que identifique la imagen de pregunta
-    console.log(imageName);
 
-    let tiempoInicio = Date.now(); // Obtener el tiempo actual
+function revisarSeleccion(){
+  let jugador1Gana = false
+  let jugador2Gana = false
 
-    // Establecer un temporizador para cambiar a una imagen aleatoria después de 0.2 segundos
-    setTimeout(function () {
-        const randomIndex = Math.floor(Math.random() * imagenes.length);
+  switch(jugador1){
+    case 'q':
+      pop.play();
+      seleccion1 = 'oveja'
+      break
+    
+    case "w" :
+      pop.play();
+      seleccion1 = 'vaca'
+      break
+
+    case "e":
+      pop.play();
+      seleccion1 = 'pollo'
+  }
+
+  switch(jugador2){
+    case 'ArrowLeft':
+      pop.play();
+      seleccion2 = 'oveja'
+      break
+    
+    case "ArrowDown" :
+      pop.play();
+      seleccion2 = 'vaca'
+      break
+
+    case "ArrowRight":
+      pop.play();
+      seleccion2 = 'pollo'
+  }
+
+  console.log(seleccion1 + "ademas de " +seleccion2)
+
+  if(seleccion1 === imageName){
+    jugador1Gana = true
+    
+
+  }
+
+  if(seleccion2 === imageName){
+    jugador2Gana = true
+    
+  
+  }
+
+  if(jugador1Gana && jugador2Gana){
+    jugador1Gana = false
+    jugador2Gana = false
+    jugador1 = null
+    jugador2 = null
+    tiempo = tiempo*0.95
+    vidaRestante = 100
+    vida.style.width = `${vidaRestante}%`
+    iniciarJuego()
+  }else if(jugador1Gana && !jugador2Gana){
+    juegoActivo = false
+    setPRecord.innerHTML = "Jugador 2";
+    perdiste()
+    console.log('Jugador 2 pierde')
+    
+  }else if(!jugador1Gana && jugador2Gana){
+    juegoActivo = false
+    setPRecord.innerHTML = "Jugador ";
+    perdiste()
+  }else{
+    juegoActivo = false
+    setPRecord.innerHTML = "Ambos perdieron";
+    perdiste()
+  }
+}
+
+function mostrarImagen(){
+  const imagenElement = document.querySelector(".display img");
+  imagenElement.src = "./IMG/pregunta.png";
+  imageName = 'pregunta';
+
+  setTimeout(
+    ()=>{
+      const randomIndex = Math.floor(Math.random() * imagenes.length);
         imagenElement.src = imagenes[randomIndex].url;
         imageName = imagenes[randomIndex].name;
         console.log(imageName);
-
-        // Establecer un nuevo intervalo después de cambiar la imagen aleatoria
-        intervalId = setInterval(function () {
-            let tiempoActual = Date.now();
-            let tiempoTranscurrido = tiempoActual - tiempoInicio;
-
-            if (tiempoTranscurrido >= tiempoEleccion) {
-                // Si ha pasado el tiempo de espera y no se ha seleccionado nada
-                if (imageName === 'bomba') {
-                    // Si la imagen es la bomba, el usuario gana un punto
-                    puntos++;
-                    setPuntos.innerHTML = puntos;
-                    console.log('¡Ganaste un punto! Puntos: ' + puntos);
-                } else {
-                    // Si la imagen no es la bomba, el usuario pierde
-                    perdiste();
-                }
-
-                // Cambiar a una nueva imagen aleatoria
-                const newRandomIndex = Math.floor(Math.random() * imagenes.length);
-                imagenElement.src = imagenes[newRandomIndex].url;
-                imageName = imagenes[newRandomIndex].name;
-                console.log(imageName);
-
-                // Reiniciar el tiempo de inicio
-                tiempoInicio = Date.now();
-            }
-
-        }, 100); // Verificar cada 100 milisegundos (ajustar según sea necesario)
-    }, 200);
+    },200
+  )
 }
 
 
 
-function VerificarImagen(id) {
-    reiniciarIntervalo(); // Reiniciar el intervalo al seleccionar una opción correcta
-
-    // Almacenar el ID original antes de eliminar el "2"
-    const idOriginal = id;
-
-    // Eliminar el "2" del final del id si está presente
-    id = id.endsWith('2') ? id.slice(0, -1) : id;
-
-    if (id == imageName) {
-        puntos++;
-        setPuntos.innerHTML = puntos;
-        console.log('Respuesta correcta! Puntos: ' + puntos);
-        tiempoEleccion = tiempoEleccion * 0.95;
-        cambiarImagenAleatoria();
-    } else {
-        botonPerdido = idOriginal;
-        perdiste();
-    }
+function iniciarJuego(){
+  
+  if(juegoActivo){
+    console.log("mostrado imagen")
+    revisarTiempo()
+  }else{
+    console.log('juego parado')
+  }
 }
-
-function reiniciarIntervalo() {
-    clearInterval(intervalId); // Detener el intervalo actual
-}
-
-
 
 function perdiste() {
-    console.log("perdiste");
+  jaja.play();
 
-    // Verificar si botonPerdido termina en 2
-    if (botonPerdido && botonPerdido.endsWith('2')) {
-        setPRecord.innerHTML = "jugador 2";
-    } else {
-        setPRecord.innerHTML = "jugador 1";
-    }
-
-    setPuntos.innerHTML = 0;
-    juegoActivo = false;
-    perdisteSection.classList.remove('oculto');
-
-    // Reiniciar botonPerdido a undefined
-    botonPerdido = undefined;
-
-    jaja.play()
+  perdisteSection.classList.remove('oculto'); 
 }
 
-
+function cerrar() {
+  perdisteSection.classList.add('oculto');
+  juegoActivo = true
+  clearInterval(revisando)
+  vidaRestante = 100
+  vida.style.width = `${vidaRestante}%`
+  iniciarJuego()
+}
 
 iniciarJuego();
